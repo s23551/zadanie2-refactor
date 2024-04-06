233928d8a262f654516722b3b31008d452abe588
s23551 <s23551@pjwstk.edu.pl>
2024-04-07 00:22:17 +0200

    Add FakeUserValidator and tests.

---
+++ LegacyAppTests/FakeUserValidatorTests.cs
@@ -0,0 +1,19 @@
+﻿using LegacyApp;
+
+namespace LegacyAppTests;
+
+public class FakeUserValidatorTests
+{
+    [Fact]
+    public void FakeUserValidator_Should_Return_True()
+    {
+        //Arrange
+        var validator = new FakeUserValidator();
+        
+        //Act
+        var result = validator.Validate(null, null, null, DateTime.Now, -1);
+        
+        //Assert
+        Assert.True(result);
+    }
+}
