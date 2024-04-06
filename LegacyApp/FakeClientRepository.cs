053a6d885c3db010a63d18b66c6ac522088fff47
s23551 <s23551@pjwstk.edu.pl>
2024-04-06 20:24:31 +0200

    Add tests for ClientMapper.

--- LegacyApp/FakeClientRepository.cs
+++ LegacyApp/FakeClientRepository.cs
@@ -10,6 +10,42 @@ public class FakeClientRepository : IClientRepository
         Email = "Test@Test.pl",
         Type = "TestClient"
     };
+
+    public static Client NORMAL_CLIENT = new Client()
+    {
+        Name = "Aron",
+        Address = "Brok",
+        ClientId = 802,
+        Email = "Czarny@Dym.es",
+        Type = "NormalClient"
+    };
+
+    public static Client IMPORTANT_CLIENT = new Client()
+    {
+        Name = "Felicjan",
+        Address = "Głuchołazy",
+        ClientId = 803,
+        Email = "Hak@Inaczej.jp",
+        Type = "ImportantClient"
+    };
+
+    public static Client VERY_IMPORTANT_CLIENT = new Client()
+    {
+        Name = "Katarzyna",
+        Address = "Lublin",
+        ClientId = 804,
+        Email = "mala@natemat.org",
+        Type = "VeryImportantClient"
+    };
+
+    public static Client UNKNOWN_CLIENT = new Client()
+    {
+        Name = "Hilary",
+        Address = "Hrebenne",
+        ClientId = 0,
+        Email = "hhrabio@hoga.hu",
+        Type = "Unknown"
+    };
     
     public Client GetById(int id)
     {
