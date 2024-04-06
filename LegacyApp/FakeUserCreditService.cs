814acf5bae9071de3d0165a74f20f12d9809901b
s23551 <s23551@pjwstk.edu.pl>
2024-04-06 23:16:14 +0200

    Add FakeUserCreditService and tests.

---
+++ LegacyApp/FakeUserCreditService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LegacyApp;
+
+public class FakeUserCreditService : IUserCreditService
+{
+    public static string OPTION_ZERO = "ZERO";
+    public static int CREDIT_DEFAULT = 1000;
+    public static string OPTION_NULL = "null";
+    
+    public int GetCreditLimit(string lastName, DateTime dateOfBirth)
+    {
+        if (lastName.Equals(OPTION_NULL))
+        {
+            throw new ArgumentException("No user in database");
+        }
+
+        return lastName.Equals(OPTION_ZERO) ? 0 : 1000;
+    }
+}
