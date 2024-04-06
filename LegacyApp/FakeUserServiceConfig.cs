19e47c4f2f2e2c72aba880060147dc75b791703b
s23551 <s23551@pjwstk.edu.pl>
2024-04-07 00:32:05 +0200

    Add FakeUserServiceConfig.

---
+++ LegacyApp/FakeUserServiceConfig.cs
@@ -0,0 +1,29 @@
+﻿namespace LegacyApp;
+
+public class FakeUserServiceConfig : IUserServiceConfig
+{
+    public IUserValidator UserValidator()
+    {
+        return new FakeUserValidator();
+    }
+
+    public IClientRepository ClientRepository()
+    {
+        return new FakeClientRepository();
+    }
+
+    public IClientMapper ClientMapper()
+    {
+        return new ClientMapper();
+    }
+
+    public IUserCreditService UserCreditService()
+    {
+        return new FakeUserCreditService();
+    }
+
+    public IUserDataAccessAdapter UserDataAccessAdapter()
+    {
+        return new FakeUserDataAccessAdapter();
+    }
+}
